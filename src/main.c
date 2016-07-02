077906c4f91e741661d54d5299726778075388ed
Weston Dransfield <wdrans@gmail.com>
2016-07-02 15:34:14 -0700

    Added background

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <pebble.h>
 
-static Window *s_main_window;
-static TextLayer *s_time_layer;
+static Window *s_main_window;  // Main window
+static TextLayer *s_time_layer;// Text for showing time
+static GFont s_time_font;      // Font 
+
+static BitmapLayer *s_background_layer; // Layer for Bitmap
+static GBitmap *s_background_bitmap;    // The background Bitmap
+
+
 
 // Function to update the time shown on the watch face
 static void update_time() {
@@ -36,18 +42,45 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_42));
+  
+  text_layer_set_font(s_time_layer, s_time_font);
+  
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   // Update the time from the start
   update_time();
 
+  // Add the Background
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACK);
+  
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+  
+  // Set the bitmap onto the layer and add to the window
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+  
+  // Add the time                                                    
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+  
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+  
 }
 
 static void init() {
@@ -66,6 +99,9 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  
+  // Make the background match the image
+  window_set_background_color(s_main_window, GColorBlack);
 }
 
 static void deinit() {
